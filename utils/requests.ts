e403b1b140fb7a7bad95b2982b154250e5de32e8
haruojmlbsolucoes <gabriel.haruo2016@gmail.com>
2022-04-27 17:35:00 -0300

    Iniciando projeto

--- utils/requests.ts
+++ utils/requests.ts
@@ -3,7 +3,7 @@ const BASE_URL = 'https://api.themoviedb.org/3'
 
 const requests = {
   fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=pt-BR`,
-  fetchNetflixOriginals: `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`,
+  fetchNetflixOriginals: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=pt-BR&with_networks=213`,
   fetchTopRated: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=pt-BR`,
   fetchActionMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=pt-BR&with_genres=28`,
   fetchComedyMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=pt-BR&with_genres=35`,
